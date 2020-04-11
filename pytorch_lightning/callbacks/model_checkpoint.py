b10102541df902f83d79f25d508f7f13203a9924
J. Borovec <jirka.borovec@seznam.cz>
2020-04-11 21:56:12 +0200

    fix import

--- pytorch_lightning/callbacks/model_checkpoint.py
+++ pytorch_lightning/callbacks/model_checkpoint.py
@@ -13,7 +13,7 @@ import numpy as np
 
 from pytorch_lightning import _logger as log
 from pytorch_lightning.callbacks.base import Callback
-from pytorch_lightning.utilities import rank_zero_warn
+from pytorch_lightning.utilities import rank_zero_warn, rank_zero_only
 
 
 class ModelCheckpoint(Callback):
